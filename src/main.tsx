09b761cd4a621337757d5db939cf1ba95e0693e7
Fewilks <felipewilks@gmail.com>
2026-09-18 11:24:08 -0300

    Install Pokemon API interceptor in main.tsx
    Added Pokemon API interceptor installation to main entry point.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { installPokemonApiInterceptor } from './services/pokemonLocalApi';
+
+installPokemonApiInterceptor();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
